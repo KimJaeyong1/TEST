b457946a6e62b419d8052dca9d69203394afa0f0
ShenWoo <95727730+KimJaeyong1@users.noreply.github.com>
2022-01-10 17:44:29 +0900

    Add files via upload

--- login.js
+++ login.js
@@ -6,13 +6,26 @@ var inputPw;
 var inputLoginBt;
 var inputSignBt;
 var inputMa;
+var now = new Date();
+var month = now.getMonth() +1;
+
+    document.write(
+        now.getUTCFullYear()+"년 "+
+        month+"월 "+
+        now.getDate()+"일 "+
+        now.getHours()+"시 "+
+        now.getMinutes()+"분 "+
+        now.getSeconds()+"초 "+
+        now.getUTCMilliseconds()+"밀리초"
+    );
 
 window.onload = function() {
     inputId = document.getElementById("loginId");
     inputPw = document.getElementById("loginPw");
-    loginBt = document.getElementById("loginButton");
-    signupBt = document.getElementById("singupButton");
+    inputLoginBt = document.getElementById("loginButton");
+    inputSignBt = document.getElementById("singupButton");
     inputMa = document.getElementById("loginMain");
+    mainContent = document.getElementById("loginMain")
 
 }
 
@@ -24,7 +37,7 @@ function login() {
     } if(inputId.value != myID && inputPw.value == myPassword) {
         alert("아이디가 틀렸습니다.")
     } if(inputId.value != myID && inputPw.value != myPassword) {
-    alert("로그인 실패")
+        alert("로그인 실패")
     }
 }
 
